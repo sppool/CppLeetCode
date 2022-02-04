7589bad9ea5fe7557cd9e8990c675401da62d9f8
Pool <sppool.me98g@NCTU.edu.tw>
2022-02-04 14:51:12 +0800

    add 53

---
+++ CppLeetCode/53. Maximum Subarray.h
@@ -0,0 +1,39 @@
+//Runtime: 100 ms, faster than 87.84 % of C++ online submissions for Maximum Subarray.
+//Memory Usage: 67.8 MB, less than 11.58 % of C++ online submissions for Maximum Subarray.
+
+#pragma once
+
+#include <iostream>
+using namespace std;
+#include <vector>
+
+class Solution
+{
+public:
+	int maxSubArray(vector<int>& nums)
+	{
+		int n = nums.size();
+
+		int sum = nums[0];
+		int tmp = nums[0];
+		for (int i = 1; i < n; i++)
+		{
+			if (sum < 0)
+			{
+				sum = nums[i];
+			}
+			else
+			{
+				sum += nums[i];
+			}
+
+			if (sum > tmp)
+			{
+				tmp = sum;
+			}
+		}
+
+		return tmp;
+	}
+};
+
