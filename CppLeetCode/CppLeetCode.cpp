654f4bf74d768e64521b996d204e4e8eaefa7185
Alan <sppool@hotmail.com>
2022-02-11 12:52:36 +0800

    re

--- CppLeetCode/CppLeetCode.cpp
+++ CppLeetCode/CppLeetCode.cpp
@@ -5,17 +5,13 @@ using namespace std;
 #include <vector>
 #include <bitset>
 #include <map>
-#include "1588. Sum of All Odd Length Subarrays.h"
+
 
 int main()
 {
 	cout << "============================ Start ============================" << endl;
-	//vector<int> v = vector<int>{ 1, 4, 2, 5, 3 };
-	Solution ans;
-	//ans.CreatArr();
-	ans.SubArr(7, 1);
-	ans.SubArr(7, 3);
-	ans.SubArr(7, 5);
-	ans.SubArr(7, 7);
+
+	//Solution ans;
+
 	cout << "============================= End =============================" << endl;
 }
